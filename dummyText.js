6d93f9e9ee0aa188980efcf946f2aa619f486d7a
Erik Shumkov <erikshumkov@gmail.com>
2020-04-05 16:40:20 +0200

    Updates

---
+++ dummyText.js
@@ -0,0 +1,107 @@
+export const slideContent = [
+  {
+    name: "Johnny Doe",
+    content: "Distillery leggings succulents, tattooed austin shaman truffaut plaid 3 wolf moon thundercats. Slow-carb kickstarter freegan, man braid food truck neutra dreamcatcher."
+  },
+  {
+    name: "Jenny Doe",
+    content: "Blog cred pork belly humblebrag intelligentsia. Hella organic coloring book, plaid vaporware quinoa crucifix swag food truck chambray pabst truffaut raw denim master cleanse. Authentic vegan squid bicycle rights raclette."
+  },
+  {
+    name: "Jack Johnson",
+    content: "Quinoa 3 wolf moon palo santo yuccie. Chillwave knausgaard normcore man braid gentrify roof party cred try-hard pabst leggings intelligentsia messenger bag."
+  },
+  {
+    name: "Nellie Michael",
+    content: "Green juice +1 mustache coloring book helvetica paleo post-ironic hexagon craft beer trust fund deep v. Woke neutra sriracha street art tbh keytar, craft beer ennui austin lo-fi lyft quinoa godard crucifix."
+  },
+  {
+    name: "Bart William",
+    content: "Kale chips narwhal copper mug etsy seitan intelligentsia, 90's tilde venmo hexagon. Art party mustache palo santo banjo street art pour-over. Photo booth jean shorts flexitarian kickstarter offal squid snackwave."
+  },
+  {
+    name: "Sadie Rollins",
+    content: "Hot chicken meditation umami, echo park quinoa kinfolk lomo seitan readymade semiotics vegan PBR&B flannel la croix. Succulents jianbing raclette migas."
+  }
+];
+
+export const menuContent = [
+  [
+    {
+      name: "Caesar Salad",
+      desc: "Amet consectetur adipisicing elit"
+    },
+    {
+      name: "Beef Salad",
+      desc: "Ratione omnis eveniet excepturi"
+    },
+    {
+      name: "BBQ Ribs",
+      desc: "vel omnis, qui odio nihil officiis quas"
+    },
+    {
+      name: "Striploin Steak",
+      desc: "Odit expedita sapiente omnis dolorum pariatur"
+    },
+    {
+      name: "Tacos",
+      desc: "Eius corrupti magni minima esse"
+    },
+    {
+      name: "BBQ Bacon Cheddar Burger",
+      desc: "Debitis, aperiam nesciunt tenetur eveniet libero"
+    }
+  ],
+  [
+    {
+      name: "Striploin Steak",
+      desc: "Odit expedita sapiente omnis dolorum pariatur"
+    },
+    {
+      name: "Ribeye Steak",
+      desc: "Odio autem deleniti tenetur"
+    },
+    {
+      name: "Lamb With Garlic Butter",
+      desc: "Earum similique fuga nostrum quaerat"
+    },
+    {
+      name: "Surf`N Turf",
+      desc: "Quibusdam accusantium voluptas esse quis incidunt"
+    },
+    {
+      name: "Sitting Bull",
+      desc: "Nihil deserunt vero, fugit perspiciatis"
+    },
+    {
+      name: "Bacon Wrapped Tenderloin",
+      desc: "Id nisi voluptates voluptas iste velit"
+    }
+  ],
+  [
+    {
+      name: "Heineken",
+      desc: "0% alcohol"
+    },
+    {
+      name: "Miller",
+      desc: "lager (4,7%)"
+    },
+    {
+      name: "Sitting Bulldog",
+      desc: "ipa (6.4%)"
+    },
+    {
+      name: "Columbia Winery, Merlot",
+      desc: "muddled blackberry, ripe black currants, blueberry"
+    },
+    {
+      name: "Cypress Vineyards Chardonnay",
+      desc: "lemon, stone fruit, vanilla aromas, red apple"
+    },
+    {
+      name: "Canyon Road, Merlot",
+      desc: "black cherry, cassis aromas, vanilla, mint, nuts"
+    }
+  ]
+];
