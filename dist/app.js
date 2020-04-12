5b81bad58ae34efd5fb185aa12a7fe60c0659562
Erik Shumkov <erikshumkov@gmail.com>
2020-04-12 22:20:28 +0200

    new animations

--- dist/app.js
+++ dist/app.js
@@ -34,6 +34,28 @@ generateMenuTxt(0);
 
 setTestimonialTxt(slide);
 
+// GSAP animation on the header
+var tl = gsap.timeline();
+tl.from(".jumbo h1", { opacity: 0, duration: .8, y: -70 }, "+=.5");
+tl.from(".book", { opacity: 0, duration: .8, y: -90 }, "-=.4");
+
+// Animation on the menu component
+function imageAnimation(src) {
+  gsap.to(menuImg, {
+    opacity: 0, x: -60, duration: .5, onComplete: () => {
+      menuImg.src = `./img/${src}.jpg`;
+    }
+  });
+  gsap.to(menuImg, { opacity: 1, x: 0, delay: .6, duration: .7 });
+}
+
+// Animation on testimonial component
+function testimonialAnimation() {
+  gsap.to(".citat", { opacity: 0, duration: .2 });
+  gsap.to(".citat", { x: -20, duration: 0, delay: .3 });
+  gsap.to(".citat", { opacity: 1, x: 0, duration: 1, delay: .5 });
+}
+
 // Event Listeners
 slideBtn.addEventListener("click", e => {
   const click = e.target;
@@ -46,13 +68,16 @@ slideBtn.addEventListener("click", e => {
     click.classList.add("selected");
 
     if (number === 1) {
-      setTestimonialTxt(number - 1);
+      testimonialAnimation();
+      setTimeout(() => setTestimonialTxt(number - 1), 500);
     }
     if (number === 2) {
-      setTestimonialTxt(number);
+      testimonialAnimation();
+      setTimeout(() => setTestimonialTxt(number), 500);
     }
     if (number === 3) {
-      setTestimonialTxt(number + 1);
+      testimonialAnimation();
+      setTimeout(() => setTestimonialTxt(number + 1), 500);
     }
   }
 });
@@ -68,16 +93,23 @@ theMenu.addEventListener("click", e => {
       document.querySelector("p.select").className = "";
       document.querySelector(".food p").className = "select";
 
-      if (menuImg.getAttribute("src").includes("wine")) {
-        menuImg.classList.add("fade");
-        setTimeout(() => {
-          menuImg.setAttribute("src", "./img/main.jpg");
-          generateMenuTxt(0);
-          menuImg.classList.add("fade2");
-        }, 500)
-        setTimeout(() => menuImg.className = "", 800);
+      // gsap.to(".hide", { x: 0, duration: 1 });
+      // gsap.to(".hide", { x: 440, duration: 1, delay: 1.1 });
+
+      if (menuImg.src.includes("dinner")) {
+        generateMenuTxt(0);
+
+        // Animate image
+        imageAnimation("main");
+      }
+
+      if (menuImg.src.includes("wine")) {
+        generateMenuTxt(0);
+
+        // Animate image
+        imageAnimation("main");
       } else {
-        menuImg.setAttribute("src", "./img/main.jpg");
+        // setTimeout(() => generateMenuTxt(0), 1100);
         generateMenuTxt(0);
       }
     }
@@ -85,16 +117,23 @@ theMenu.addEventListener("click", e => {
       document.querySelector("p.select").className = "";
       document.querySelector(".food p").className = "select";
 
-      if (menuImg.getAttribute("src").includes("wine")) {
-        menuImg.classList.add("fade");
-        setTimeout(() => {
-          menuImg.setAttribute("src", "./img/main.jpg");
-          generateMenuTxt(1);
-          menuImg.classList.add("fade2");
-        }, 500)
-        setTimeout(() => menuImg.className = "", 800);
+      // gsap.to(".hide", { x: 0, duration: 1 });
+      // gsap.to(".hide", { x: 440, duration: 1, delay: 1.1 });
+      if (menuImg.src.includes("main")) {
+        generateMenuTxt(1);
+
+        // Animate image
+        imageAnimation("dinner");
+      }
+
+      if (menuImg.src.includes("wine")) {
+        generateMenuTxt(1);
+
+        // Animate image
+        imageAnimation("dinner");
+
       } else {
-        menuImg.setAttribute("src", "./img/main.jpg");
+        // setTimeout(() => generateMenuTxt(1), 1100);
         generateMenuTxt(1);
       }
     }
@@ -104,25 +143,19 @@ theMenu.addEventListener("click", e => {
     e.target.classList.add("select");
     [...menuCategory.children].forEach(head => head.classList.remove("select"));
     menuCategory.children[0].classList.add("select");
-    menuImg.classList.add("fade");
-    setTimeout(() => {
-      menuImg.setAttribute("src", "./img/main.jpg");
-      generateMenuTxt(0);
-      menuImg.classList.add("fade2");
-    }, 500)
-    setTimeout(() => menuImg.className = "", 800);
+    generateMenuTxt(0);
+
+    // Animate image
+    imageAnimation("main");
   }
   if (e.target.innerText === "Drinks and Wine") {
     [...menuCategory.children].forEach(head => head.classList.remove("select"));
     document.querySelector("p.select").className = "";
     e.target.classList.add("select");
-    menuImg.classList.add("fade");
-    setTimeout(() => {
-      menuImg.setAttribute("src", "./img/wine.jpg");
-      generateMenuTxt(2);
-      menuImg.classList.add("fade2");
-    }, 500)
-    setTimeout(() => menuImg.className = "", 800);
+    generateMenuTxt(2);
+
+    // Animate image
+    imageAnimation("wine");
   }
 });
 
